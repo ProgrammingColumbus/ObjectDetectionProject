a8735883b197f66d5270315eeef4e6268ca36b28
ProgrammingColumbus <103267295+ProgrammingColumbus@users.noreply.github.com>
2022-12-21 18:35:24 +0700

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,52 @@
+//Start
+
+img = "";
+status1 = "";
+objects = [];
+
+function preload(){
+    img = loadImage("fruits.jpg");
+}
+
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetect = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status:  Detecting Objects.";
+
+}
+
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(status1 != ""){
+        for(i = 0; i< objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object(s) detected.";
+
+            fill("#A085AD");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "  " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#0854AD");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("Model: Initialized.");
+    status1 = true;
+    objectDetect.detect(img, gotResult);
+
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+
+    console.log(results);
+    objects = results;
+}
